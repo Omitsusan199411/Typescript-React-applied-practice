098adacaa63c0b9f03416f2afc346f932fc3e2ad
Omitsusan199411 <59048819+Omitsusan199411@users.noreply.github.com>
2022-05-29 21:00:54 +0900

    axiosの取得データに型を指定 <Array<TodoType>>

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
 import axios from "axios";
 import React, { useState } from "react";
+import { Todo } from "./Todo";
 import "./styles.css";
+
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
 export const App = () => {
   // useStateをany型で指定
-  const [todos, setTodos] = useState<any>([]);
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
   const onClickFetchData = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      setTodos(res.data);
-    });
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
   };
   return (
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
-        <p>{todo.title}</p>
+        <Todo title={todo.title} userid={todo.userId} />
       ))}
     </div>
   );
